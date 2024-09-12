023c561accc34cd10d031acf0f0896c2d6a4c67e
Kozlov Ilya <kozlov.ilya.code@gmail.com>
2024-09-12 10:18:54 +0300

    Set initial bot configuration

---
+++ bot/src/index.ts
@@ -0,0 +1,33 @@
+import { Bot, GrammyError, HttpError } from 'grammy';
+
+const bot = new Bot(process.env.BOT_API_TOKEN);
+
+bot.command('start', async ctx => {
+  await ctx.reply('Bot started!');
+});
+
+bot.command('hello', async ctx => {
+  await ctx.reply('Greetings, traveler!');
+});
+
+bot.api.setMyCommands([
+  { command: 'start', description: 'Старт бота' },
+  { command: 'hello', description: 'Приветствие' },
+]);
+
+bot.catch(err => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+
+  const e = err.error;
+
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+});
+
+bot.start();
